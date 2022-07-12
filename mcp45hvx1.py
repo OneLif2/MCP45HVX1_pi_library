3cd7d5ae5ecd990125c735a02c09d8d8973845c3
Onelife <d2nnis@hotmail.com>
2022-07-12 18:18:48 +0800

    update license

--- mcp45hvx1.py
+++ mcp45hvx1.py
@@ -1,11 +1,24 @@
-# ******************************************************
-#  Raspberry Pi / Jetson Nano library for MCP45HVX1 digital potentiometers
-#
-#  Author: OneLife d2nnis@hotmail.com
-#
-#  Version: 1.0.0
-#  License: Apache 2.0
-# *******************************************************
+""" ******************************************************
+
+  This file is part of the MCP41HVX1 library.
+  Copyright (c) 2022 OneLif2. All rights reserved.
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+********************************************************** """
+
 import smbus
 import time
 
